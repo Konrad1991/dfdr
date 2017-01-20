d2f2612ad0bfb12ca73d6442b38afd90a43a3134
Thomas Mailund <mailund@birc.au.dk>
2017-01-20 16:16:33 +0100

    failing but need to move on now

--- tests/testthat/test-simpification.R
+++ tests/testthat/test-simpification.R
@@ -1,6 +1,6 @@
 context("Expression simplification")
 
-test_that("We can simplify numeric expressions", {
+test_that("we can simplify numeric expressions", {
   expect_equal(simplify_expr(quote(1)), 1)
   expect_equal(simplify_expr(quote(0)), 0)
 
@@ -22,4 +22,19 @@ test_that("We can simplify numeric expressions", {
   expect_equal(simplify_expr(quote(5 ** 0)), 1)
   expect_equal(simplify_expr(quote(5 ** 1)), 5)
   expect_equal(simplify_expr(quote(2 ** 2)), 4)
+})
+
+test_that("we can do some simplifications when there are variables involved", {
+  expect_equal(simplify_expr(quote(x)), quote(x))
+  expect_equal(simplify_expr(quote(x + 0)), quote(x))
+  expect_equal(simplify_expr(quote(0 + x)), quote(x))
+  expect_equal(simplify_expr(quote(x + x)), quote(x + x)) # we don't actually simplify something like this...
+
+  expect_equal(simplify_expr(quote(x - 0)), quote(x))
+  expect_equal(simplify_expr(quote(0 - x)), quote(0 - x))
+  expect_equal(simplify_expr(quote(x - x)), quote(x - x)) # we don't actually simplify something like this...
+
+  expect_equal(simplify_expr(quote(1 * x)), quote(x))
+  expect_equal(simplify_expr(quote(x * 1)), quote(x))
+  expect_equal(simplify_expr(quote(x / 1)), quote(x))
 })
